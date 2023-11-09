a407292b82e5b079afb77ff767be7c797b198abd
Allen <329988300@qq.com>
2023-11-09 23:18:53 +0800

    init

--- packages/renderer/src/routes.ts
+++ packages/renderer/src/routes.ts
@@ -2,12 +2,17 @@ import { lazy } from 'solid-js';
 import type { RouteDefinition } from 'solid-app-router';
 
 import Home from './pages/home';
+import Qr from './pages/qr';
 import AboutData from './pages/about.data';
 
 export const routes: RouteDefinition[] = [
   {
     path: '/',
     component: Home,
+  },
+  {
+    path: '/qr',
+    component: Qr,
   },
   {
     path: '/about',
