25853c402644f34b5571cb839326c5b835655078
张傲 <za413588@alibaba-inc.com>
2024-08-14 11:19:53 +0800

    fix: 修改细节

--- packages/main/lib/getQrCode.ts
+++ packages/main/lib/getQrCode.ts
@@ -3,13 +3,20 @@ const base64 = require("base-64");
 import getIpAddress from "./getIpAddress";
 
 function getQrCode(payload: any) {
-  const { port } = payload;
+  const { port, app } = payload;
   return new Promise((resolve, reject) => {
-    const protocol = "http://";
-    const ipAddress = getIpAddress();    
-    const configStr = base64.encode(`${ipAddress}:${port}`);
+    let protocol = app === "shadowrocket" ? "http://" : "socks5://";
+    const ipAddress = getIpAddress();
+    const configStr =
+      app === "shadowrocket"
+        ? base64.encode(`${ipAddress}:${port}`)
+        : `${ipAddress}:${port}`;
+
+    const url = `${protocol}${configStr}#proxySS`;
+    console.log(url);
+    
     QRCode.toDataURL(
-      `${protocol}${configStr}`,
+      url,
       { type: "terminal" },
       function (err: any, url: string) {
         resolve(url);
--- packages/renderer/src/pages/home.tsx
+++ packages/renderer/src/pages/home.tsx
@@ -1,23 +1,78 @@
-import { createSignal } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 
 export default function Home() {
-  const [list, setList] = createSignal<any[]>([]);
-  window.ipcRenderer.on("proxy-item-add", (_, { data }) => {
-    console.log(data);
-    setList(list().concat(data));
+  const [port, setPort] = createSignal(8888);
+  const [ipAddress, setIpAddress] = createSignal("127.0.0.1");
+  const [qrCodeUrl, setQrCodeUrl] = createSignal("");
+  const [isSuccess, setIsSuccess] = createSignal(false);
+  const [app, setApp] = createSignal("sagernet");
+
+  onMount(() => {
+    handleGenerAnd();
   });
+
+  const initIpAddress = () => {
+    window.ipcRenderer.on("send-ip-address", (_, payload) => {
+      console.log(payload.ipAddress);
+
+      setIpAddress(payload.ipAddress);
+    });
+    window.ipcRenderer.send("get-ip-address");
+  };
+  const handleGenerIOS = () => {
+    setPort(8888);
+    handleGener("shadowrocket");
+  };
+  const handleGenerAnd = () => {
+    setPort(8889);
+    handleGener("sagernet");
+  };
+  const handleGener = (app: string) => {
+    setApp(app);
+    window.ipcRenderer.send("get-qr-code-url", {
+      port: port(),
+      app,
+    });
+    window.ipcRenderer.on("send-qr-code-url", (_event, payload) => {
+      setQrCodeUrl(payload.url);
+      setIsSuccess(true);
+    });
+  };
+
+  initIpAddress();
+
   return (
     <section>
-      这是vv-proxy
-      <section class="proxy-list">
-        {list().map((item) => (
-          <div>
-            {/* <div>{JSON.stringify(item)}</div> */}
-            <output>Url: {item.url}</output>
-            <output>Host: {item.host}</output>
-            <output>Method: {item.method}</output>
-          </div>
-        ))}
+      <section>
+        自动获取局域网地址，预制两个端口，默认生成的Sagernet(Android常用)二维码！
+      </section>
+      <section class="bg-gray-100 text-gray-700 p-8">
+        <div class="flex items-center space-x-2">
+          <output>{ipAddress()}:</output>
+          <output>{port()}</output>
+          <button
+            class="border rounded-lg px-2 border-gray-900"
+            onClick={handleGenerIOS}
+          >
+            shadowrocket
+          </button>
+          <button
+            class="border rounded-lg px-2 border-gray-900"
+            onClick={handleGenerAnd}
+          >
+            sagernet
+          </button>
+        </div>
+        <div class="flex items-center space-x-2">
+          {isSuccess() ? (
+            <label>
+              {app()}端口{port()}生成成功
+            </label>
+          ) : null}
+        </div>
+      </section>
+      <section>
+        <img src={qrCodeUrl()} />
       </section>
     </section>
   );
